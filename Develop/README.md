55efdcf96865c1f791589542d9aefd329c57e786
Ruby Hill <163611320+slym187@users.noreply.github.com>
2024-06-13 14:45:59 -0500

    worked on index and markdown

---
+++ Develop/README.md
@@ -0,0 +1,19 @@
+# readme
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description
+readme file
+
+## Installation
+fill out form
+
+## Usage
+usage
+
+## License
+
+This project is licensed under the MIT license. For more information, please visit [this link](https://opensource.org/licenses/MIT).
+
+## Questions
+For questions or concerns about this project, please contact [slym187](https://github.com/slym187) via email at meldrina91@yahoo.com.
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badges = {
+    'MIT': '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)',
+    'GNU GPLv3': '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+    'Apache 2.0': '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)',
+    'ISC': '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)',
+    'None': ''
+  };
+  return badges[license] || '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const links = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'GNU GPLv3': 'https://www.gnu.org/licenses/gpl-3.0',
+    'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'ISC': 'https://opensource.org/licenses/ISC',
+    'None': ''
+  };
+  return links[license] || '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `## License
+
+This project is licensed under the ${license} license. For more information, please visit [this link](${renderLicenseLink(license)}).`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+For questions or concerns about this project, please contact [${data.username}](https://github.com/${data.username}) via email at ${data.email}.
 `;
 }
 
