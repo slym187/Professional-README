eb333990a4e7dacb9b1b3e4d0da915869cb70fc1
Ruby Hill <163611320+slym187@users.noreply.github.com>
2024-06-17 17:01:30 -0500

    worked on markdown and index

--- Develop/README.md
+++ Develop/README.md
@@ -1,19 +1,36 @@
-# readme
+# h
 
 ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
 ## Description
-readme file
+h
 
 ## Installation
-fill out form
+h
 
 ## Usage
-usage
+h
+
+## Contribution
+h
+
+## Tests
+h
 
 ## License
 
 This project is licensed under the MIT license. For more information, please visit [this link](https://opensource.org/licenses/MIT).
 
+
+
 ## Questions
-For questions or concerns about this project, please contact [slym187](https://github.com/slym187) via email at meldrina91@yahoo.com.
+For questions or concerns about this project, please contact [h](https://github.com/h) via email at h.
--- Develop/index.js
+++ Develop/index.js
@@ -1,71 +1,84 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path');
+const { generateMarkdown, generateHtml } = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'What is the name of your project?',
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Enter a description of your project:',
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'Enter installation instructions:',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'Enter usage information:',
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: 'Choose a license for your project:',
-    choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None'],
-  },
-  {
-    type: 'input',
-    name: 'username',
-    message: 'Enter your GitHub username:',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'Enter your email address:',
-  },
-];
-
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Enter contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) => {
-    if (err) {
-      console.error('Error writing README file:', err);
-    } else {
-      console.log('README file has been successfully created!');
-    }
-  });
-}
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+      } else {
+        console.log(`${fileName} has been successfully created!`);
+      }
+    });
+  }
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer
-    .prompt(questions)
-    .then((answers) => {
-      const readmeContent = generateMarkdown(answers);
-      writeToFile('README.md', readmeContent);
-    })
-    .catch((error) => {
-      console.error('Error occurred:', error);
-    });
-}
+    inquirer
+      .prompt(questions)
+      .then((answers) => {
+        const markdownContent = generateMarkdown(answers);
+        const htmlContent = generateHtml(markdownContent);
+        
+        writeToFile('README.md', markdownContent);
+        writeToFile('README.html', htmlContent);
+      })
+      .catch((error) => {
+        console.error('Error occurred:', error);
+      });
+  }
 
 // Function call to initialize app
 init();
